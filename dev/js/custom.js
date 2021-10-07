726d78dfe0f4475af235a9784c91d7489f8230cb
a.golchenko <a.golchenko@sharksw.com>
2021-10-07 13:09:55 +0300

    styling header

--- dev/js/custom.js
+++ dev/js/custom.js
@@ -1,33 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+// -- open header menu on mouse over --//
 
-    /**
-     * Fixes vh unit in touch devices
-     *
-     * @since   1.0
-     */
-    const setupVh = () => {
-
-        const setVh = () => {
-
-            // get the viewport height and we multiple it by 1% to get a value for a vh unit
-            let vh = window.innerHeight * 0.01;
-
-            // set the value in the --vh custom property to the root of the document
-            document.documentElement.style.setProperty(`--vh`, `${vh}px`);
-
-        }
-
-        setVh();
-        window.addEventListener(`resize`, setVh, false);
-
+$("details").hover(
+    function () {
+        $(this).attr('open', true);
+    },
+    function () {
+        $(this).attr('open', false);
     }
-
-    /**
-     * Chocks away! 
-     */  
-    setupVh();
-
-});
-
-
-import '/test'
+);
